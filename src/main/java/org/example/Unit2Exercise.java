de33e0b1f8b7c86bd6f10cb00fe5fa7c1d8dea1e
Gaurish Kamat <gaurish.kamat@t-systems.com>
2024-11-18 23:11:39 +0530

    adding new class

---
+++ src/main/java/org/example/Unit2Exercise.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Unit2Exercise {
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Gaurish", "Kamat", 35),
+                new Person("Girish", "Kamat", 35),
+                new Person("Pooja", "Kamat", 31),
+                new Person("Moiez", "Shaikh", 31)
+        );
+
+        for(Person p: people){
+            System.out.println(p);
+        }
+    }
+}
