a6d70e0cc44324149a57d4ab924647fb01bcc130
Gaurish Kamat <gaurish.kamat@t-systems.com>
2024-11-19 23:47:29 +0530

    variables

---
+++ src/main/java/kg/coding/challenges/Variables.java
@@ -0,0 +1,20 @@
+package kg.coding.challenges;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myNumber = 865;
+        System.out.println(myNumber);
+
+        float myFloat = 5.0F;
+        System.out.println(myFloat);
+
+        double myDouble = 3.14;
+        System.out.println(myDouble);
+
+        boolean isVegetarian = true;
+        System.out.println(isVegetarian);
+
+        String myString = "Good Morning!!";
+        System.out.println(myString);
+    }
+}
