cfbfb2acfa95178b0eb5ef70a17d4acedf9f6ae1
Gaurish Kamat <gaurish.kamat@t-systems.com>
2024-11-24 23:55:17 +0530

    change in logic

--- ProgrammingChallenge/src/main/java/org/example/18. LeapYear.java
+++ ProgrammingChallenge/src/main/java/org/example/18. LeapYear.java
@@ -8,7 +8,9 @@ class LeapYear {
         System.out.print("Enter a year to check if it is a leap year: ");
         int year = input.nextInt();
 
-        if(year%4 == 0){
+        if(year % 4 == 0 && year % 100 != 0){
+            System.out.println("It is a leap year");
+        }else if(year % 100 == 0 && year % 400 == 0){
             System.out.println("It is a leap year");
         }else{
             System.out.println("It is not a leap year");
