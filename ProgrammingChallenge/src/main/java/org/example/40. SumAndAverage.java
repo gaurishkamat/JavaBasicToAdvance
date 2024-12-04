8f0c1ff52faef8ce6390853c6d69e4067e2237c4
Gaurish Kamat <gaurish.kamat@t-systems.com>
2024-12-04 22:05:00 +0530

    array challenges

--- ProgrammingChallenge/src/main/java/org/example/40. SumAndAverage.java
+++ ProgrammingChallenge/src/main/java/org/example/40. SumAndAverage.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 class SumAndAverage {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+        int i = 0;
+        int sum = 0;
+        while(i < numbers.length){
+            sum += numbers[i];
+            i++;
+        }
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + (float) sum/numbers.length);
     }
 }
