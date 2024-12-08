e0cd76f6d28ba09b92496841ee23b7f1f9630f2a
Gaurish Kamat <gaurish.kamat@t-systems.com>
2024-12-08 23:53:45 +0530

    array challenges

--- ProgrammingChallenge/src/main/java/org/example/45. ReverseArray.java
+++ ProgrammingChallenge/src/main/java/org/example/45. ReverseArray.java
@@ -4,9 +4,23 @@ class ReverseArray {
     public static void main(String[] args) {
         System.out.println("Welcome to reverse array");
         int[] array = ArrayUtility.inputArray();
+
+        int[] reverseArr = reverseArray(array);
+        System.out.println("Reverse array is: ");
+        ArrayUtility.displayArray(reverseArr);
     }
 
     public static int[] reverseArray(int[] arr){
-
+        int i = 0;
+        int j = arr.length - 1;
+        int temp = 0;
+        while(i <= j){
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        return arr;
     }
 }
