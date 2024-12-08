7aa8c3d7aafd513233a98c52a2f7256dacaea07a
Gaurish Kamat <gaurish.kamat@t-systems.com>
2024-12-09 00:18:07 +0530

    array challenges

--- ProgrammingChallenge/src/main/java/org/example/46. IsPalindromeArray.java
+++ ProgrammingChallenge/src/main/java/org/example/46. IsPalindromeArray.java
@@ -2,6 +2,29 @@ package org.example;
 
 class IsPalindromeArray {
     public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome array");
+        int[] arr = ArrayUtility.inputArray();
 
+        boolean isPalindrome = checkPalindrome(arr);
+
+        if(isPalindrome){
+            System.out.println("Given array is Palindrome");
+        }else{
+            System.out.println("Given array is not Palindrome");
+        }
+    }
+
+    public static boolean checkPalindrome(int[] arr){
+        int i = 0;
+        int j = arr.length - 1;
+
+        while(i <= j){
+            if(arr[i] != arr[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
