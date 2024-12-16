c460860b2fba1b6f899904eff5955313facc8728
Gaurish Kamat <gaurish.kamat@t-systems.com>
2024-12-16 23:40:16 +0530

    for loop and more

---
+++ ProgrammingChallenge/src/main/java/org/example/66. ContinueAdd.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+class ContinueAdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] arr = ArrayUtility.inputArray();
+
+        int sum = addArray(arr);
+        System.out.println("Total of numbers: " +sum);
+    }
+
+    public static int addArray(int[] arr){
+        int sum = 0;
+        for(int item: arr){
+            if(item < 0){
+                continue;
+            }
+            sum+= item;
+        }
+        return  sum;
+    }
+}
