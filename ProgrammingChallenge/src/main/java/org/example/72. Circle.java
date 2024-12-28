a965f6ac447c388fe0dc6efcb266b87e922f4851
Gaurish Kamat <gaurish.kamat@t-systems.com>
2024-12-28 23:10:27 +0530

    dice and random guess

--- ProgrammingChallenge/src/main/java/org/example/72. Circle.java
+++ ProgrammingChallenge/src/main/java/org/example/72. Circle.java
@@ -3,11 +3,25 @@ package org.example;
 import java.util.Scanner;
 
 class Circle {
+    double radiusInMm;
+
+    Circle(double radius){
+        this.radiusInMm = radius;
+    }
+
+    double getCircumference(){
+        return 2 * Math.PI * radiusInMm;
+    }
+
+    double getArea(){
+        return Math.PI * Math.pow(radiusInMm, 2);
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter radius of circle: ");
         int r = input.nextInt();
-        System.out.println("Area of circle: " + (Math.PI * r * r));
-        System.out.println("Circumference of circle: " + (2 * Math.PI * r));
+        Circle circle = new Circle(r);
+        System.out.println("Area of circle: " + circle.getArea());
+        System.out.println("Circumference of circle: " + circle.getCircumference());
     }
 }
