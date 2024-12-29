7eb5884fb19ce7371a4e83c16f8477275d9c6ecb
Gaurish Kamat <gaurish.kamat@t-systems.com>
2024-12-29 23:22:56 +0530

    final keyword

--- ProgrammingChallenge/src/main/java/org/example/75. ArrayToString.java
+++ ProgrammingChallenge/src/main/java/org/example/75. ArrayToString.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.util.Scanner;
-
 class ArrayToString {
     public static void main(String[] args) {
         String[] arr = new String[] {"This", "is", "the", "best", "course", "for", "Java",
