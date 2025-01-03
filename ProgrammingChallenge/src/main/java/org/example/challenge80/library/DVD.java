bfbc68bc602872236607017d1c2fb03c1aed8f0d
Gaurish Kamat <gaurish.kamat@t-systems.com>
2025-01-03 22:33:06 +0530

    inheritance

--- ProgrammingChallenge/src/main/java/org/example/challenge80/library/DVD.java
+++ ProgrammingChallenge/src/main/java/org/example/challenge80/library/DVD.java
@@ -3,11 +3,22 @@ package org.example.challenge80.library;
 public class DVD extends LibraryItem{
     private int durationInMins;
 
+    public DVD(int durationInMins) {
+        this.durationInMins = durationInMins;
+    }
+
     public int getDurationInMins() {
         return durationInMins;
     }
 
     public void setDurationInMins(int durationInMins) {
         this.durationInMins = durationInMins;
+    }
+
+    @Override
+    public String toString() {
+        return "DVD{" +
+                "durationInMins=" + durationInMins +
+                '}';
     }
 }
