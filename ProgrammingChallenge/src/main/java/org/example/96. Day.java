ed5a019ec441dbcd1fd62681de683f65e16ffa58
gaurishkamat <gaurishkamat23@gmail.com>
2025-01-27 23:39:10 +0530

    Enumerations

---
+++ ProgrammingChallenge/src/main/java/org/example/96. Day.java
@@ -0,0 +1,28 @@
+package org.example;
+
+enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
+
+
+class EnumTest{
+    public static void main(String[] args) {
+        System.out.println(Day.MONDAY);
+        System.out.println(Day.TUESDAY);
+        System.out.println(Day.WEDNESDAY);
+        System.out.println(Day.THURSDAY);
+        System.out.println(Day.FRIDAY);
+        System.out.println(Day.SATURDAY);
+        System.out.println(Day.SUNDAY);
+
+        for (Day day : Day.values()) {
+            System.out.println(value);
+        }
+    }
+}
