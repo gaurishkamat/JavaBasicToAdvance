6e995870f090578908ba93fc2219994440ad4c52
gaurishkamat <gaurishkamat23@gmail.com>
2025-02-02 23:51:57 +0530

    Streams

--- ProgrammingChallenge/src/main/java/org/example/challenge108/ConcatenateStream.java
+++ ProgrammingChallenge/src/main/java/org/example/challenge108/ConcatenateStream.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class ConcatenateStream {
     public static void main(String[] args) {
-        List<String> namelist = List.of("Virat Kohli", "Rohit Sharma", "Axar Patel", "M S Dhoni", "Ravi Jadeja");
+        List<String> namelist = List.of("Virat Kohli", "R Sharma", "Axar Patel", "M S Dhoni", "Ravi Jadeja");
 
         String concatenatedString = namelist.stream()
                 .filter(name -> name.length() >= 10)
