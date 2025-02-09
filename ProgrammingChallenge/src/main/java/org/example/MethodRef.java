65e0b41d2e8dd2a0f9743c1d0aac805cb0b52ce6
gaurishkamat <gaurishkamat23@gmail.com>
2025-02-09 22:26:52 +0530

    Method References

--- ProgrammingChallenge/src/main/java/org/example/MethodRef.java
+++ ProgrammingChallenge/src/main/java/org/example/MethodRef.java
@@ -6,6 +6,10 @@ public class MethodRef {
     public static void main(String[] args) {
         List<String> names = List.of("Moiez", "Girish", "Gaurish");
 
+        StringBuffer str = new StringBuffer("Java Code");
+        System.out.println(str.capacity());
+        str.trimToSize();
+        System.out.println(str.capacity());
         names.stream()
                 .map(String::toUpperCase)
                 .forEach(System.out::println);
