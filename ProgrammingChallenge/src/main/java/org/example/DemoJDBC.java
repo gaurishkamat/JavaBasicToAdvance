bace3b6c74dc7068539642cedb6dadb5b12c142c
gaurishkamat <gaurishkamat23@gmail.com>
2025-02-12 00:06:23 +0530

    JDBC connection steps

--- ProgrammingChallenge/src/main/java/org/example/DemoJDBC.java
+++ ProgrammingChallenge/src/main/java/org/example/DemoJDBC.java
@@ -13,12 +13,13 @@ public class DemoJDBC {
         Connection connection = DriverManager.getConnection(url, username, password);
         System.out.println("Connection established");
         Statement statement = connection.createStatement();
-        String query = "select sname from student where sid = 1";
+        String query = "select * from student";
         ResultSet set = statement.executeQuery(query);
 
 
-        System.out.println(set.next());
-        System.out.println(set.getString("sname"));
+        while (set.next()){
+            System.out.print(set.getInt("sid") + " " + set.getString("sname") + " " + set.getInt("marks") + "\n" );
+        }
 
         connection.close();
         System.out.println("Connection closed");
