f5065acc378a2113e40ff67508935991a9c1efe8
gaurishkamat <gaurishkamat23@gmail.com>
2025-02-11 23:57:17 +0530

    JDBC connection steps

--- ProgrammingChallenge/src/main/java/org/example/DemoJDBC.java
+++ ProgrammingChallenge/src/main/java/org/example/DemoJDBC.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 
 public class DemoJDBC {
@@ -11,8 +12,18 @@ public class DemoJDBC {
         String username = "postgres";
         String password = "2305";
         Connection connection = DriverManager.getConnection(url, username, password);
-
         System.out.println("Connection established");
+        Statement statement = connection.createStatement();
+        String query = "select sname from student where sid = 1";
+        ResultSet set = statement.executeQuery(query);
+
+
+        System.out.println(set.next());
+        System.out.println(set.getString("sname"));
+
+        connection.close();
+        System.out.println("Connection closed");
+
 
     }
 }
