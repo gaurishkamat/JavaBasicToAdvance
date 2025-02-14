48e4ed623f36564563510da2553bff7e7632e18a
gaurishkamat <gaurishkamat23@gmail.com>
2025-02-14 22:24:49 +0530

    Standalone hibernate

---
+++ JavaHibernate/src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Student student = new Student();
+
+        student.setRollNo(91);
+        student.setName("Girish");
+        student.setAge(36);
+
+        Class.forName("org.postgresql.Driver");
+
+        SessionFactory sessionFactory = new Configuration()
+                .configure()
+                .addAnnotatedClass(org.example.Student.class)
+                .buildSessionFactory();
+
+
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(student);
+
+        transaction.commit();
+        session.close();
+
+    }
+}
