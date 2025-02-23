4c084fd4e7d7098fe48fe570a0ae0469061f2e92
gaurishkamat <gaurishkamat23@gmail.com>
2025-02-23 18:45:17 +0530

    Spring MVC

--- SpringBootWeb1/src/main/java/com/gaurish/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/gaurish/SpringBootWeb1/HomeController.java
@@ -1,9 +1,9 @@
 package com.gaurish.SpringBootWeb1;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -13,12 +13,13 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(HttpServletRequest req, HttpSession session){
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
+    public String add(@RequestParam("num1") int num1,@RequestParam("num2") int num2, Model model){
+//        int num1 = Integer.parseInt(req.getParameter("num1"));
+//        int num2 = Integer.parseInt(req.getParameter("num2"));
+
         int result = num1 + num2;
 
-        session.setAttribute("result", result);
+        model.addAttribute("result", result);
         return "result.jsp";
     }
 }
--- SpringBootWeb1/src/main/webapp/result.jsp
+++ SpringBootWeb1/src/main/webapp/result.jsp
@@ -2,6 +2,6 @@
 
 <html>
     <body>
-        <h2>Result is: <%= session.getAttribute("result")%></h2>
+        <h2>Result is: ${result}</h2>
     </body>
 </html>
