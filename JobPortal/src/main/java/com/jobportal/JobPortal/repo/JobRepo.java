b5e132f0f3afbf45fe21c38bbc421727505d53d1
gaurishkamat <gaurishkamat23@gmail.com>
2025-02-26 22:35:17 +0530

    view and modal changes

--- JobPortal/src/main/java/com/jobportal/JobPortal/repo/JobRepo.java
+++ JobPortal/src/main/java/com/jobportal/JobPortal/repo/JobRepo.java
@@ -9,10 +9,11 @@ import java.util.List;
 public class JobRepo {
 
     List<JobPost> jobs = new ArrayList<>(Arrays.asList(
-            new
-    ))
+            new JobPost(102, "React Dev", "Let him code", 2, List.of("A", "B")),
+            new JobPost(103, "Angular Dev", "Let him also code", 2, List.of("A", "B"))
+    ));
 
     public List<JobPost> getAllJobs(){
-
+        return jobs;
     }
 }
