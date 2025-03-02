8d098ab4d0c7d00b569cc5152cc7c399b7b3eb41
gaurishkamat <gaurishkamat23@gmail.com>
2025-03-02 10:08:40 +0530

    adding more jobs

--- JobPortal/src/main/java/com/jobportal/JobPortal/repo/JobRepo.java
+++ JobPortal/src/main/java/com/jobportal/JobPortal/repo/JobRepo.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class JobRepo {
 
     List<JobPost> jobs = new ArrayList<>(Arrays.asList(
-            new JobPost(1, "React Dev", "Let him code", 2, List.of("A", "B")),
+            new JobPost(1, "  Dev", "Let him code", 2, List.of("A", "B")),
             new JobPost(2, "Angular Dev", "Let him also code", 2, List.of("M", "B")),
             new JobPost(3, "Java Dev", "You are doing the backend", 2, List.of("F", "B")),
             new JobPost(4, "C# .Net Dev", "Microsoft jobs", 2, List.of("A", "B", "C")),
