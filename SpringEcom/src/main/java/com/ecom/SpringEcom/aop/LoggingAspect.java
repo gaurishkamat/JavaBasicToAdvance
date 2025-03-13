944766948fbb4d1efc27c3fe049cf009b0c692d5
gaurishkamat <gaurishkamat23@gmail.com>
2025-03-14 00:02:45 +0530

    adding aspects

--- SpringEcom/src/main/java/com/ecom/SpringEcom/aop/LoggingAspect.java
+++ SpringEcom/src/main/java/com/ecom/SpringEcom/aop/LoggingAspect.java
@@ -1,11 +1,18 @@
 package com.ecom.SpringEcom.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class LoggingAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
+            //return type class-name.method-name(args)
+            @Before("execution(* com.ecom.SpringEcom.service.ProductService.*(..))")
             public void logMethodCall(){
                 LOGGER.info("Method called");
             }
