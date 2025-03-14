9628af47c149ba61077d618988be2c97ce93e536
gaurishkamat <gaurishkamat23@gmail.com>
2025-03-14 23:45:17 +0530

    adding aspects before, after and around

--- SpringEcom/src/main/java/com/ecom/SpringEcom/aop/LoggingAspect.java
+++ SpringEcom/src/main/java/com/ecom/SpringEcom/aop/LoggingAspect.java
@@ -1,5 +1,7 @@
 package com.ecom.SpringEcom.aop;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.LoggerFactory;
@@ -12,8 +14,18 @@ public class LoggingAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
             //return type class-name.method-name(args)
-            @Before("execution(* com.ecom.SpringEcom.service.ProductService.*(..))")
+            @Before("execution(* com.ecom.SpringEcom.service.ProductService.getAllProducts(..))")
             public void logMethodCall(){
                 LOGGER.info("Method called");
+            }
+
+            @After("execution(* com.ecom.SpringEcom.service.ProductService.getAllProducts(..)")
+            public void logMethodExecuted(){
+                LOGGER.info("Method executed");
+            }
+
+            @Around("execution(* com.ecom.SpringEcom.service.ProductService.getAllProducts(..)")
+            public void logMethodTime(){
+                LOGGER.info("Method time");
             }
 }
