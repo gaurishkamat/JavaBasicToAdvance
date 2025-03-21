9bae3223323de07dc0d08b6f252512e9614ac8dc
gaurishkamat <gaurishkamat23@gmail.com>
2025-03-22 00:23:23 +0530

    adding security

--- SpringBootRest/src/main/java/com/jobportal/SpringBootRest/repo/JobRepository.java
+++ SpringBootRest/src/main/java/com/jobportal/SpringBootRest/repo/JobRepository.java
@@ -4,8 +4,6 @@ import com.jobportal.SpringBootRest.model.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Repository
--- SpringBootRest/src/main/java/com/jobportal/SpringBootRest/service/MyUserDetailsService.java
+++ SpringBootRest/src/main/java/com/jobportal/SpringBootRest/service/MyUserDetailsService.java
@@ -17,8 +17,6 @@ public class MyUserDetailsService implements UserDetailsService {
 
     public UserDetails loadUserByUsername(String username){
         User user = userRepo.findByUsername(username);
-        System.out.println(user.getUsername());
-        System.out.println(user.getPassword());
         if(user == null){
             System.out.println("User not found");
             throw new UsernameNotFoundException("User not found");
